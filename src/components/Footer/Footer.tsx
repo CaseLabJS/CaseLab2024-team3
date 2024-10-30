1628557ca7f10e76b72cbb8c09cb693de99cf8a1
AksiniaFox <aksinia.fox@gmail.com>
2024-10-30 21:21:29 +0300

    fix: :bug: Fix doubled css

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-[#E8EAF6] h-[70px] bg-bg-footer">
+    <footer className="bg-[#E8EAF6] h-[70px]">
       <div className="mx-auto max-w-screen-xl p-4">
         <span className="text-sm sm:text-center text-gray-400">© 2024 <a href="#" className="hover:underline">CaseLab team 3</a></span>
       </div>
